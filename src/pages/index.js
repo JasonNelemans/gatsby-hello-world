be7bb6432e49338935f30f075591746eecd9fdb0
JasonNelemans <jasonnelemans@hotmail.com>
2020-06-16 18:08:54 +0200

    allFiles query

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,58 @@ import React from "react"
 import Link from "gatsby-link"
 import { graphql } from 'gatsby'
 import Layout from "../components/Layout"
-import Counter from "./Counter"
+
 
 export default function Home({data}) {
   return (
       <Layout>
-        <h1>{data.site.siteMetadata.title}</h1>
-        <p>This is a paragraph</p>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }) => 
+                <tr>
+                  <td>
+                    {node.relativePath}
+                  </td>
+                  <td>
+                    {node.prettySize}
+                  </td>
+                  <td>
+                    {node.extension}
+                  </td>
+                  <td>
+                    {node.birthTime}
+                  </td>
+                </tr>
+            )}
+          </tbody>
+        </table>
         <Link to="/page-2/">Page 2</Link>
         <br />
         <Link to="/dir1/page-3/">Page 3</Link>
         <br />
-        <Counter color="blue" />
+        <Link to="/Counter/">Counter</Link>
       </Layout>
   )
 }
 
 export const query = graphql`
-  query FirstQuery {
-    site {
-      siteMetadata {
-        title
-        author
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
       }
     }
   }
